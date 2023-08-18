dabf966b89c0f7f119a770ad920c2b526f6340a6
NhuY29 <100512487+NhuY29@users.noreply.github.com>
2023-08-18 16:35:58 +0700

    Tạo page login

--- First/src/App.vue
+++ First/src/App.vue
@@ -2,10 +2,11 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import layout from './components/layout/layout1.vue'
+import login from './components/login/login.vue'
 </script>
 
 <template>
- <layout></layout>
+ <login></login>
 </template>
 
 <style scoped>
