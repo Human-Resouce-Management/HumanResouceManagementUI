c1ca8844d2fb24bb68103dd398bfdfc2d5200192
NhuY29 <100512487+NhuY29@users.noreply.github.com>
2023-08-19 13:55:50 +0700

    Tạo layout

--- HumanResouceManagement/src/App.vue
+++ HumanResouceManagement/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import layout1 from './components/layout/layout1.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,7 +18,8 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView /> -->
+  <layout1></layout1>
 </template>
 
 <style scoped>
--- HumanResouceManagement/src/main.ts
+++ HumanResouceManagement/src/main.ts
@@ -1,11 +1,18 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+// app.use(router)
 
+// app.mount('#app')
+app.use(router)
+app.use(ElementPlus)
+// app.use(Toast)
 app.mount('#app')
